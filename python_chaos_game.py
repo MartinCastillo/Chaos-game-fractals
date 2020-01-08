eeba53e546ffcf5996b7e7fec97b677ff46b2bcf
Martín <43865973+MartinCastillo@users.noreply.github.com>
2020-01-07 22:07:57 -0300

    resuelve ese bug de guardado

--- python_chaos_game.py
+++ python_chaos_game.py
@@ -11,20 +11,20 @@ from math import ceil , sqrt
 import os ; import datetime
 from random import sample
 #Params
-image_size = 1500; #500x500
-n_points = 100; #Número de puntos a dibujar
+image_size = 800; #500x500
+n_points = 300; #Número de puntos a dibujar
 point_size = 1; #Tamaño de punto
-prop = 2/3; #Proporción de división de distancia entre puntos
+prop = 1/2; #Proporción de división de distancia entre puntos
 repetir_vertice = True; #Puede repetir el mismo vertice dos veces seguidas
-save = True
+save = False
 save_path = "/res"
 #vértices de la forma
-shape_coords = [[0,image_size//2],[image_size,image_size],[image_size,0]]
-#shape_coords = [[0,0],[0,image_size],[image_size,0],[image_size,image_size]]
+shape_coords = [[0,image_size],[image_size,image_size],[image_size//2,0]]
+"""shape_coords = [[0,0],[0,image_size],[image_size,0],[image_size,image_size]]"""
 """shape_coords = [[image_size//2,0],[0,image_size//2],[image_size//4,image_size],\
     [3*image_size//4,image_size],[image_size,image_size//2]]"""
 
-current_path = os.getcwd()
+current_path = os.getcwd().replace("\\",'/')
 coord_act = shape_coords[0] #Punto actual
 vertex = 0;
 img = np.zeros((image_size,image_size,3), np.uint8)
@@ -55,12 +55,13 @@ if (__name__ == '__main__'):
         if(k == ord('q')):
             cv2.destroyAllWindows()
             break
+    cv2.imshow('w',img)
+    cv2.waitKey(0)
     if(save):
         print(str(current_path+save_path))
         os.chdir(current_path+save_path)
-        filename = "puntos={};vertices={};fraccion={};repetir_vertice={};;{}.jpg".format(n_points,\
-            len(shape_coords),prop,repetir_vertice,datetime.datetime.now())
+        filename = "points={};vertex={};fractions={};repeat_vertex={};size{}.jpg".format(n_points,
+            len(shape_coords),prop,repetir_vertice,image_size)
         cv2.imwrite(filename, img)
         print('Successfully saved at ' + current_path+save_path)
-    cv2.imshow('w',img)
-    cv2.waitKey(0)
+    pass
